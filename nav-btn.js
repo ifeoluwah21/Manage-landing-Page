08e8bf8f554f154a5630850c84f53dc2915f7653
Ifeoluwah <spencerife.21@gmail.com>
2022-08-26 21:21:20 +0100

    readme and small fix

--- nav-btn.js
+++ nav-btn.js
@@ -4,9 +4,11 @@ export const openNavBtn = document.querySelector(".header__menu-btn");
 export const closeNavBtn = document.querySelector(".header__close-btn");
 export function open() {
     nav.dataset.visible = "true";
+    nav.ariaHidden = false;
     openNavBtn.style.display = "none"
 }
 export function close() {
     nav.dataset.visible = "";
+    nav.ariaHidden = true;
     openNavBtn.style.display = "block"
 }
